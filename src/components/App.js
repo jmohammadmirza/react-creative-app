3021323846d74965278a05f37d83bde7a89c0e42
Mohammad Javed <mdaslameme@gmail.com>
2019-10-07 11:56:50 +0400

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+import Footer from './Footer';
+
+import { without } from 'lodash';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      formDisplay: false,
+      orderBy :'patientName',
+      orderDir:'asc',
+      queryText:''
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  searchApts(query) {
+    this.setState({ queryText: query });
+  }
+  changeOrder(order,  dir){
+  this.setState({
+    orderBy:order,
+    orderDir:dir
+  });
+  }
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+     tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts
+     });
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
+  componentDidMount() {
+    fetch('https://outpatient-api.herokuapp.com/outpatient/fetch')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        return item;
+      })
+      this.setState({
+        myAppointments: apts
+      });
+    });
+
+  }
+
+  render() {
+
+    let order;
+    let filterApps=this.state.myAppointments;
+    if(this.state.orderDir==='asc'){
+      order=1;
+    } else{
+      order=-1;
+    }
+   filterApps=filterApps.sort((a, b) => {
+      if (
+        a[this.state.orderBy].toLowerCase() <
+        b[this.state.orderBy].toLowerCase()
+      ) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    })
+    .filter(eachItem => {
+    return (
+      eachItem['patientName']
+      .toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+      eachItem['appointmentDate']
+      .toLowerCase().includes(this.state.queryText.toLowerCase()) ||
+      eachItem['comments']
+      .toLowerCase().includes(this.state.queryText.toLowerCase())
+    );
+    });
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                  toggleForm={this.toggleForm}
+                  addAppointment={this.addAppointment}
+                />
+               <SearchAppointments
+                 orderBy={this.state.orderBy}
+                 orderDir={this.state.orderDir}
+                 changeOrder={this.changeOrder}
+                 searchApts={this.searchApts}
+               />
+                <ListAppointments
+                  appointments={filterApps}
+                  deleteAppointment={this.deleteAppointment}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+    {/*  <Footer/> */}
+      </main>
+    );
+  }
+}
+
+export default App;
